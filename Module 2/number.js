f74e9057de200bb7c5e0dca121bb4710d09ffe35
Janarthanan667 <133737479+Janarthanan667@users.noreply.github.com>
2023-09-26 14:44:57 +0530

    Add files via upload

---
+++ Module 2/number.js
@@ -0,0 +1,16 @@
+console.log("number in Javascript!!");
+
+var age =24;
+var salary =25000;
+var bodyWeight =50;
+var content=936108879;
+var randomNumber = 1000.475;
+var negativeNumber = -90;
+
+console.log(typeof age);
+console.log(typeof salary);
+console.log(typeof bodyWeight);
+console.log(typeof content);
+console.log(typeof randomNumber);
+console.log(typeof negativeNumber);
+console.log(typeof 0);
---
+++ Module 2/number2.js
@@ -0,0 +1,30 @@
+//tostring()
+//toExponential()
+//toFixed()
+//toprecision()
+//valueof()
+
+var x=120;
+console.log(x);
+console.log(x.toString());
+console.log((120+80).toString());
+
+var z=9.76599999;
+console.log(z);
+
+var e=4.678;
+console.log(e.toExponential(5));
+
+var y=8.97699;
+console.log(y.toFixed(2));
+
+var c=8.675;
+console.log(c.toPrecision(2));
+console.log((20*12).valueOf());
+
+var age ='44';
+
+console.log(Number(age));
+console.log(Number(new Date("2020-01-02")));
+console.log(parseInt("44"));
+console.log(parseFloat("23457867.4774"));
