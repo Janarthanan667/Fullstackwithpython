4400962246292f4bf163587e3a533b23ba9083ff
Janarthanan667 <133737479+Janarthanan667@users.noreply.github.com>
2023-09-26 12:43:48 +0530

    Add files via upload

---
+++ Module 2/Gettingstartedwith.js
@@ -0,0 +1,25 @@
+// usual print
+console.log("Gettingstartedwith.js");
+console.warn("how are u");
+console.error("jet");
+
+//1.)programming
+//2.)handling your views
+//declaration
+//initialization
+var age;//declaration
+var bag ='Books';
+var fullName ='jana';
+bag="tiffan box";
+var count =20+20;
+var englishmarks =92;
+var tamilmarks=100;
+console.log(englishmarks + tamilmarks);
+console.log(count);
+console.log(bag);
+age = 20;
+console.log(age);
+console.log(fullName);
+//---->for controlling your html
+//---->controlling your forms,paro,heading,buttons
+//----->DOM
